5676c01bb57bad97ea4e6c44611383cbf5a875ed
Olfran Jiménez <olfran@gmail.com>
2016-12-03 23:14:08 -0400

    Added Chapter 1 - Exercise 1.3

---
+++ ch1/1.3.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+;-------------------------------------------------------------------------------
+; Exercise 1.3. Define a procedure that takes three numbers as arguments
+; and returns the sum of the squares of the two larger numbers.
+;-------------------------------------------------------------------------------
+
+(define (square n) (* n n))
+(define (sum-squares a b) (+ (square a) (square b)))
+(define (sum-larger-squares a b c)
+  (cond ((and (<= a b) (<= a c)) (sum-squares b c))
+        ((and (<= b a) (<= b c)) (sum-squares a c))
+        (else (sum-squares a b))))
+
+(display (sum-larger-squares 2 4 3)) (newline)
